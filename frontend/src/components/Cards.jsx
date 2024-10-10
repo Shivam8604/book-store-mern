538c975eecd4b58cbb71d52e2a3ed4018d6e3221
Shivam pal <devshivampal360@gamil.com>
2024-10-10 16:51:28 +0530

    First Commit

---
+++ frontend/src/components/Cards.jsx
@@ -0,0 +1,29 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const Cards = ({item}) => {
+
+    return (
+        <div>
+            <div className="card bg-base-100 w-96 shadow-xl mt-4">
+                <figure>
+                    <img src={item.image} alt="Shoes" />
+                </figure>
+                <div className="card-body">
+                    <h2 className="card-title">
+                        {item.name}
+                        <div className="badge badge-secondary">{item.category}</div>
+                    </h2>
+                    <p>{item.title}</p>
+                    <div className="card-actions justify-between">
+                        <div className="badge badge-outline">{item.price}</div>
+                        <div className="badge badge-outline px-2 py-1 cursor-pointer hover:text-white bg-pink-600 p-3 duration-200 ">Buy Now</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Cards
